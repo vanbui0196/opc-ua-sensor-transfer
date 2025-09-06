3ad2724c6dcc5ed3e7e35f5b94bd676a584bc5f3
Bui Khanh Van <van.buikhanh@hotmail.com>
2025-09-06 10:34:18 +0100

    update build script

---
+++ sw_build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+rm -rf build
+mkdir build
+
+# CMake generation
+cmake . -Bbuild
+
+# CMake build folder
+cmake --build build
+
+# copy the file from build to current location
+cp build/opcua_server server
